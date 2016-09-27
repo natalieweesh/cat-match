7d8f77b0c35bc61a02c3a26adfd65d0518ac0d70
Natalie Wee <natalie@key.me>
2016-09-27 16:32:10 -0400

    Move cat images to folder

--- src/Tile.js
+++ src/Tile.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Tile = React.createClass({
   render() {
-    return <div className="tile" data-num={this.props.uniqueid} data-value={this.props.num} onClick={this.props.clickTile.bind(null, this.props.uniqueid, this.props.num)} style={{'backgroundImage': this.props.currentlyShowingIds.indexOf(this.props.uniqueid) !== -1 || this.props.finalIds.indexOf(this.props.uniqueid) !== -1 ? 'url(cat-' + this.props.num + '.jpg)' : 'none'}}>
+    return <div className="tile" data-num={this.props.uniqueid} data-value={this.props.num} onClick={this.props.clickTile.bind(null, this.props.uniqueid, this.props.num)} style={{'backgroundImage': this.props.currentlyShowingIds.indexOf(this.props.uniqueid) !== -1 || this.props.finalIds.indexOf(this.props.uniqueid) !== -1 ? 'url(img/cat-' + this.props.num + '.jpg)' : 'none'}}>
       {this.props.num}
     </div>
   }
